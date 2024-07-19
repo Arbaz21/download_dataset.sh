79e9b05d862d0c889b8bc076fb19ba7d4aef65df
Arbaz21 <62392544+Arbaz21@users.noreply.github.com>
2024-07-19 11:25:00 +0500

    Create download_dataset.sh

---
+++ download_dataset.sh
@@ -0,0 +1,3 @@
+wget -P data/full_dataset/ https://storage.googleapis.com/gresearch/goemotions/data/full_dataset/goemotions_1.csv
+wget -P data/full_dataset/ https://storage.googleapis.com/gresearch/goemotions/data/full_dataset/goemotions_2.csv
+wget -P data/full_dataset/ https://storage.googleapis.com/gresearch/goemotions/data/full_dataset/goemotions_3.csv
